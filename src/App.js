e894390b5ac5a0ab7f7d80f8ad4321124350b4cc
feres2005 <feresbouderhem1@gmail.com>
2020-09-20 10:07:57 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
+
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import data from "./data.json";
+import './index.css'
+import Products from './components/Products';
+import Filter from './components/Filter';
+import Cart from './components/Cart';
+import store from "./store"
+import { Provider } from 'react-redux';
+ class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+<div className="grid-container">
+  <header>
+    <a href='/'>shoping Cart</a>
+  </header>
+  <main>
+    <div className="content">
+      <div className="main">
+        <Filter 
+         ></Filter>
+        <Products >
+        </Products>
+       
+        </div>
+      <div className="sidebar"> 
+     <Cart  
+       />
+        </div>
     </div>
-  );
-}
+    
+  </main>
+  <footer>all right is reserved</footer>
+</div>
+</Provider>
 
-export default App;
+    )
+  }
+}
+export default App
